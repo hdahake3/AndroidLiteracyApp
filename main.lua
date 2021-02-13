e8085017ef304cbec0a619e099620ada196b884a
kferacho3 <78626761+kferacho3@users.noreply.github.com>
2021-02-13 04:05:00 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ display.setDefault("background", 213/255, 236/255, 237/255)
 
 
 
-
+-- array of all images for letters to tap to login
 local loginKeysImages = {
 	'images/a.png',
 	'images/b.png',
@@ -50,14 +50,18 @@ local loginKeysImages = {
 
 local loginKeys = display.newGroup()
 
+--array of inputs for login charachters (5 maximum)
 local loginArray = {0, 0, 0, 0, 0}
 
+--deletes input when user taps on designated square 
 local loginDeleteButtonEvent = function(event)
 	loginArray[event.target.id] = 0
 	display.remove(event.target)
 end
 
-
+--nested for loop used for input selection
+--outer for loop is for each input
+--inner forloop controls the placement of current input imaged (mapped with image id)
 local loginButtonEvent = function(event)
 	for i = 1, #loginArray, 1 do 
 		if loginArray[i] == 0 then
@@ -74,6 +78,7 @@ local loginButtonEvent = function(event)
     print(event.target.id)
 end
 
+--for loop used to display input login keys on screen for user to tap
 for i = 1, #loginKeysImages, 1 do 
 	
 	local curImage = display.newImage(loginKeys, loginKeysImages[i], 32 + ((i - 1) % 9) * 52, 32 + math.floor((i - 1) / 9) * 52)
@@ -84,18 +89,21 @@ for i = 1, #loginKeysImages, 1 do
 
 end
 
+--displays "login" button
 loginKeys.y = loginKeys.y + 30
 
+--displays "Parent & Teacher Access" in header
 local parentTeacher = display.newImage(loginKeys, 'images/parent_teacher.png', display.contentCenterX, loginKeys.y - 45)
 parentTeacher:scale(0.3, 0.3)
 
+--for loop that displays empty character location for password input
 for i = 1, 5, 1 do
 	local curImage = display.newImage(loginKeys, 'images/empty_login.png', 40 + (i - 1) * 65, loginKeys.y + 220)
 	curImage:scale(0.3, 0.3)
 end
 
 
-
+--displays login key image in correct location
 local loginButton = display.newImage(loginKeys, 'images/login.png', loginKeys.x + 400, loginKeys.y + 220)
 loginButton:scale(0.3, 0.3)
 
